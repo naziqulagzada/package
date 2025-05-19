7d5cd630f33ea24e6a131a99b47edc684a7be2dc
naziqulagzada <naziqulagzada@gmail.com>
2025-05-19 08:39:15 +0500

    first_commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="package",
+    version="0.1",
+    description="mypackage is just a practice.",
+    author="nazia",
+    author_email="nqulagzada@gmail.com",
+    url="https://github.com/naziqulagzada/package.git",
+    license="MIT",
+    packages=["mypackage"],
+    install_requires=["requests"],
+    zip_safe=False
+
+)
